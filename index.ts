efdd3a5980c0bb34d3eadbe13373ebdae112121e
Mieszko4 <mieszko4@gmail.com>
2019-01-20 20:47:57 +0100

    Use REST GET

--- index.ts
+++ index.ts
@@ -1,68 +1,24 @@
 import { prisma } from './generated/prisma-client'
-import { GraphQLServer } from 'graphql-yoga'
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const resolvers = {
-  Query: {
-    publishedPosts(parent, args, context) {
-      return context.prisma.posts({ where: { published: true } })
-    },
-    post(parent, args, context) {
-      return context.prisma.post({ id: args.postId })
-    },
-    postsByUser(parent, args, context) {
-      return context.prisma.user({
-        id: args.userId
-      }).posts()
-    }
-  },
-  Mutation: {
-    createDraft(parent, args, context) {
-      return context.prisma.createPost(
-        {
-          title: args.title,
-          author: {
-            connect: { id: args.userId }
-          }
-        },
+const app = express()
 
-      )
-    },
-    publish(parent, args, context) {
-      return context.prisma.updatePost(
-        {
-          where: { id: args.postId },
-          data: { published: true },
-        },
+app.use(bodyParser.json())
 
-      )
-    },
-    createUser(parent, args, context) {
-      return context.prisma.createUser(
-        { name: args.name },
-      )
-    }
-  },
-  User: {
-    posts(parent, args, context) {
-      return context.prisma.user({
-        id: parent.id
-      }).posts()
-    }
-  },
-  Post: {
-    author(parent, args, context) {
-      return context.prisma.post({
-        id: parent.id
-      }).author()
-    }
-  }
-}
-
-const server = new GraphQLServer({
-  typeDefs: './schema.graphql',
-  resolvers,
-  context: {
-    prisma
-  },
+app.get(`/polls`, async (req, res) => {
+  const publishedPosts = await prisma.polls()
+  res.json(publishedPosts)
 })
-server.start(() => console.log('Server is running on http://localhost:4000'))
+
+app.get('/poll/:pollId', async (req, res) => {
+  const { pollId } = req.params
+  const post = await prisma.poll({ id: pollId })
+  const answers = await prisma.poll({ id: pollId }).answers();
+  res.json({
+    ...post,
+    answers
+  })
+})
+
+app.listen(3000, () => console.log('Server is running on http://localhost:3000'))
