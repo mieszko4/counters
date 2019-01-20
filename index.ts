03faa08ac877668b6fc763f648088d2400f7156f
Mieszko4 <mieszko4@gmail.com>
2019-01-20 17:24:10 +0100

    Add graphql

--- index.ts
+++ index.ts
@@ -1,28 +1,68 @@
 import { prisma } from './generated/prisma-client'
-import { create } from 'domain';
+import { GraphQLServer } from 'graphql-yoga'
 
-async function main() {
-  const newUser = await prisma.createUser({
-    name: 'Alice',
-    email: 'a@test.com',
-    posts: {
-      create: [
-        {
-          title: 'Post one'
-        },
-        {
-          title: 'Post two'
-        }
-      ]
+const resolvers = {
+  Query: {
+    publishedPosts(parent, args, context) {
+      return context.prisma.posts({ where: { published: true } })
+    },
+    post(parent, args, context) {
+      return context.prisma.post({ id: args.postId })
+    },
+    postsByUser(parent, args, context) {
+      return context.prisma.user({
+        id: args.userId
+      }).posts()
     }
-  })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+  },
+  Mutation: {
+    createDraft(parent, args, context) {
+      return context.prisma.createPost(
+        {
+          title: args.title,
+          author: {
+            connect: { id: args.userId }
+          }
+        },
 
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+      )
+    },
+    publish(parent, args, context) {
+      return context.prisma.updatePost(
+        {
+          where: { id: args.postId },
+          data: { published: true },
+        },
 
-  const allPosts = await prisma.posts()
-  console.log(allPosts)
+      )
+    },
+    createUser(parent, args, context) {
+      return context.prisma.createUser(
+        { name: args.name },
+      )
+    }
+  },
+  User: {
+    posts(parent, args, context) {
+      return context.prisma.user({
+        id: parent.id
+      }).posts()
+    }
+  },
+  Post: {
+    author(parent, args, context) {
+      return context.prisma.post({
+        id: parent.id
+      }).author()
+    }
+  }
 }
 
-main().catch(e => console.error(e))
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
