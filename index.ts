3287386a06f6f8d8986c166c9d4feae3cd51dc4f
Mieszko4 <mieszko4@gmail.com>
2019-01-20 21:45:33 +0100

    Add create REST

--- index.ts
+++ index.ts
@@ -3,10 +3,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-
 app.use(bodyParser.json())
 
-app.get(`/polls`, async (req, res) => {
+// wrapper for catching async errors
+const wrap = fn => (...args) => fn(...args).catch(args[2])
+
+const getPoll = async (name) => {
+  const poll = await prisma.poll({ name })
+  const answers = await prisma.poll({ name }).answers();
+
+  if (!poll) {
+    return null;
+  }
+
+  return {
+    question: poll.question,
+    published_at: poll.createdAt,
+    details: {
+      answers: answers.map(answer => ({
+        answer: answer.name,
+        counter: answer.count
+      }))
+    }
+  }
+};
+
+app.get(`/polls`, wrap(async (req, res) => {
   const polls = await prisma.polls()
   res.json({
     polls: polls.map(poll => ({
@@ -14,28 +36,33 @@ app.get(`/polls`, async (req, res) => {
       question: poll.question
     }))
   })
-})
+}))
 
-app.get('/poll/:pollName', async (req, res) => {
+app.get('/poll/:pollName', wrap(async (req, res) => {
   const { pollName } = req.params
-  const poll = await prisma.poll({ name: pollName })
-  const answers = await prisma.poll({ name: pollName }).answers();
+  const poll = await getPoll(pollName);
 
   if (!poll) {
     return res.status(404).json({});
   }
 
-  res.json({
-    question: poll.question,
-    name: poll.name,
-    published_at: poll.createdAt,
-    votes: {
-      counters: answers.map(answer => ({
-        counter: answer.name,
-        votes: answer.count
+  res.json(poll)
+}))
+
+app.post('/polls', wrap(async (req, res) => {
+  const { body } = req;
+  const newPoll = await prisma.createPoll({
+    name: body.name,
+    question: body.question,
+    answers: {
+      create: body.answers.map(answer => ({
+        name: answer
       }))
     }
   })
-})
+
+  const poll = await getPoll(newPoll.name)
+  res.json(poll)
+}))
 
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'))
---
+++ snippets/createPollWithAnswers.bash
@@ -0,0 +1,14 @@
+curl --header "Content-Type: application/json" --request POST --data '{"name":"YourLanguage2","question":"Favourite programming language?","answers":["Swift","Python","Objective-C","Ruby"]}' http://localhost:3000/polls
+
+# {
+#  "name": "YourLanguage",
+#  "question": "Favourite programming language?",
+#  "answers": [
+#    "Swift",
+#    "Python",
+#    "Objective-C",
+#    "Ruby"
+#  ]
+# }
+
+#'{"name":"YourLanguage","question":"Favourite programming language?","answers":["Swift","Python","Objective-C","Ruby"]}''
